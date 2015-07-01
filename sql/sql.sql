85672e7da9ff7aca11facc729c913ad763deb65b
kdyzm <1649653731@qq.com>
2015-07-01 21:23:10 +0800

    创建数据库和表的sql语句。

---
+++ sql/sql.sql
@@ -0,0 +1,8 @@
+create database test;
+create table
+CREATE TABLE `user` (
+   `id` varchar(32) NOT NULL,
+   `name` varchar(32) DEFAULT NULL,
+   `age` int(11) DEFAULT NULL,
+   PRIMARY KEY (`id`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8
